2d0e3abcadb147013fcc9388dd345b3aca85eb7c
Mike Austin <mike_ekim@yahoo.com>
2025-04-16 14:36:26 -0700

    Add wrap, spacing, and align View props

--- lib/components/view/View.tsx
+++ lib/components/view/View.tsx
@@ -1,8 +1,13 @@
 import clsx from "clsx";
 
-import { Color } from "../../types/Color.tsx";
+import { Spacing } from "../../types/Spacing.ts";
+import { Align } from "../../types/Align.ts";
+import { Color } from "../../types/Color.ts";
 
 import viewStyles from "./View.module.scss";
+import spacingStyles from "../../styles/spacing.module.scss";
+import alignHorizontalStyles from "../../styles/alignHorizontal.module.scss";
+import alignVerticalStyles from "../../styles/alignVertical.module.scss";
 import fillColorStyles from "../../styles/fillColor.module.scss";
 
 type Delegate<TProps, TDelegate extends React.ElementType, TOmit extends string = ""> =
@@ -11,7 +16,10 @@ type Delegate<TProps, TDelegate extends React.ElementType, TOmit extends string 
 function View<TDelegate extends React.ElementType = "div">({
   as,
   flex,
+  wrap,
   horizontal,
+  spacing,
+  align,
   fillColor,
   className,
   children,
@@ -19,7 +27,10 @@ function View<TDelegate extends React.ElementType = "div">({
 }: Delegate<{
   as?: TDelegate;
   flex?: boolean;
+  wrap?: boolean;
   horizontal?: boolean;
+  spacing?: Spacing;
+  align?: Align;
   fillColor?: Color;
 }, TDelegate>) {
   const Component = as ?? "div";
@@ -27,7 +38,14 @@ function View<TDelegate extends React.ElementType = "div">({
   const viewClassName = clsx(
     viewStyles.View,
     flex && viewStyles.flex,
+    wrap && viewStyles.wrap,
     horizontal && viewStyles.horizontal,
+    spacing && spacingStyles[`_${spacing.replace(/ /, "_")}`],
+    align && (
+      horizontal
+        ? alignHorizontalStyles[align.replace(/ /, "_")]
+        : alignVerticalStyles[align.replace(/ /, "_")]
+    ),
     fillColor && fillColorStyles[fillColor],
     className
   );
---
+++ lib/types/Spacing.ts
@@ -0,0 +1,7 @@
+type Values = "0px" | "4px" | "8px" | "12px" | "16px" | "24px" | "32px";
+
+type Spacing = `${Values}` | `${Values} ${Values}`;
+
+export {
+  type Spacing
+};
